33fb0a774dcfba9e33229c078b1e95ed1072dbe3
Eric Mordi <ericmordi@gmail.com>
2020-04-21 00:06:41 +0000

    added recipes.html test page

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId 
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'the_cocktail_guide'
+app.config["MONGO_URI"] = 'mongodb+srv://root:r00tUser@myfirstcluster-ldgtk.mongodb.net/the_cocktail_guide?retryWrites=true&w=majority'
+
+mongo = PyMongo(app)
 
 
 @app.route('/')
-def hello():
-    return 'this is the cocktail guide!!!'
+@app.route('/recipes')
+def get_tasks():
+    return render_template("recipes.html", recipe=mongo.db.recipe.find())
 
 
 if __name__ == '__main__':
