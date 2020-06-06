d0dbc07cf44b45bb89a1ee098befe0708ef2bd1a
Eric Mordi <ericmordi@gmail.com>
2020-06-06 21:55:01 +0000

    all initail updates and corrctions for all pages and secret key added

--- app.py
+++ app.py
@@ -1,17 +1,58 @@
 import os
-from flask import Flask, render_template, redirect, request, url_for
+from flask import Flask, render_template, redirect, request, session, url_for, flash 
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId 
+import bcrypt
+
+if os.path.exists("env.py"):
+    import env
 
 app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY")
 app.config["MONGO_DBNAME"] = 'the_cocktail_guide'
 app.config["MONGO_URI"] = 'mongodb+srv://root:r00tUser@myfirstcluster-ldgtk.mongodb.net/the_cocktail_guide?retryWrites=true&w=majority'
 
-mongo = PyMongo(app)
 
+mongo = PyMongo(app)
 @app.route('/')
 def index():
     return render_template("index.html")
+
+
+@app.route('/home')
+def home():
+    if 'username' in session:
+        return 'You are logged in as ' + session['username']
+
+    return render_template('home.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+    users = mongo.db.users
+    login_user = users.find_one({'name' : request.form['username']})
+
+    if login_user:
+        if bcrypt.hashpw(request.form['pass'].encode('utf-8'), login_user['password'].encode('utf-8')) == login_user['password'].encode('utf-8'):
+            session['username'] = request.form['username']
+            return redirect(url_for('index'))
+
+    return 'Invalid username/password combination'
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    if request.method == 'POST':
+        users = mongo.db.users
+        existing_user = users.find_one({'name' : request.form['username']})
+
+        if existing_user is None:
+            hashpass = bcrypt.hashpw(request.form['pass'].encode('utf-8'), bcrypt.gensalt())
+            users.insert({'name' : request.form['username'], 'password' : hashpass})
+            session['username'] = request.form['username']
+            return redirect(url_for('index'))
+        
+        return 'That username already exists!'
+
+    return render_template('register.html')
                            
 
 @app.route('/recipe')
@@ -29,10 +70,8 @@ def insert_recipe():
             'alcohol': request.form.get('alcohol'),
             'difficulty': request.form.get('difficulty'),
             'description': request.form.get('description'),
-            'ingredients': request.form.getlist('add_ingredient[]'),
-            'instructions_step_1': request.form.get('instructions_step_1'),
-            'instructions_step_2': request.form.get('instructions_step_2'),
-            'instructions_step_3': request.form.get('instructions_step_3'),
+            'ingredients': request.form.getlist('ingredients'),
+            'instructions': request.form.getlist('instructions'),
             'alcohol_rating': request.form.get('alcohol_rating'),
             'cocktail_image': request.form.get('cocktail_image'),
             'prep_time': request.form.getlist('prep_time')
@@ -68,9 +107,7 @@ def update_recipe(recipe_id):
             'difficulty': request.form.get('difficulty'),
             'description': request.form.get('description'),
             'ingredients': request.form.getlist('ingredients'),
-            'instructions_step_1': request.form.get('instructions_step_1'),
-            'instructions_step_2': request.form.get('instructions_step_2'),
-            'instructions_step_3': request.form.get('instructions_step_3'),
+            'instructions': request.form.getlist('instructions'),
             'alcohol_rating': request.form.get('alcohol_rating'),
             'cocktail_image': request.form.get('cocktail_image'),
             'prep_time': request.form.getlist('prep_time')
@@ -85,6 +122,14 @@ def add_cocktail():
     alcohols = mongo.db.alcohols
     alcohols.insert_one(alcohol)
     return redirect(url_for('recipe'))
+
+
+@app.route('/alcohols')
+def alcohols():
+    return render_template('alcohols.html',
+                           alcohols=mongo.db.alcohols.find())
+
+
 
 
 
