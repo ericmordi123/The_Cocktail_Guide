a84cc3a4e3bb7092df2635f5c7bd4ab19def4109
Eric Mordi <ericmordi@gmail.com>
2020-07-31 21:34:06 +0000

    update for deployment

--- env.py
+++ env.py
@@ -1,4 +1,4 @@
 import os 
 
 os.environ.setdefault("SECRET_KEY", "secret_flash_key")
-os.environ.setdefault("SECRET_KEY", "mongodb+srv://root:r00tUser@myfirstcluster-ldgtk.mongodb.net/the_cocktail_guide?retryWrites=true&w=majority")
+os.environ.setdefault("MONGO_URI", "mongodb+srv://root:r00tUser@myfirstcluster-ldgtk.mongodb.net/the_cocktail_guide?retryWrites=true&w=majority")
